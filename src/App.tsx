f68ec8d7b138b68613a82c8e0c589415da2c6ecd
sev <santisevitz@hotmail.com>
2025-07-07 00:11:07 -0300

    testing out pre-push action

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl flex items-center justify-center mt-52 font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl flex items-center justify-center mt-52 font-bold underline">
+        Roadmap App!
+      </h1>
     </>
   );
 }
