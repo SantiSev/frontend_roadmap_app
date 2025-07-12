bdc251392b0cb94b089612b044247784dca6bcc9
sev <santisevitz@hotmail.com>
2025-07-12 13:04:54 -0300

    adding build github action and removing old docker compose file

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ function App() {
   return (
     <>
       <h1 className="flex-col text-3xl flex items-center justify-start mt-52">
-        Deployment Test 
-        <span className="text-blue-500 text-lg font-thin">Juli es la mas linda</span>
+        Deployment Test
+        <span className="text-blue-500 text-lg font-thin">
+          Juli es la mas linda
+        </span>
       </h1>
     </>
   );
