25f2ddf0511f6d397fa77b975beeefbd8c02358c
sev <santisevitz@hotmail.com>
2025-07-12 13:08:59 -0300

    gh action test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="flex-col text-3xl flex items-center justify-start mt-52">
-        Deployment Test
+        GH action test
         <span className="text-blue-500 text-lg font-thin">
           Juli es la mas linda
         </span>
